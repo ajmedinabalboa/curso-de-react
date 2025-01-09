b04a6d20cf4470a3166c5899d9bad71808c656a9
ajmedinabalboa <alvaro.medina.developer@gmail.com>
2025-01-08 20:52:49 -0400

    add components to project

---
+++ src/Components/Home/Body.jsx
@@ -0,0 +1,14 @@
+const Body = () => {
+    return (
+        <div>
+            <h1>Vite + React</h1>
+            <h2>DESARROLLO FRONT END CON REACTJS</h2>
+            <h2>Desarrollo de Aplicaciones Web con React</h2>                          
+            <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+            </p>            
+        </div>
+    );
+};
+
+export default Body;

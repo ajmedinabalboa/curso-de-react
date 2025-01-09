9677287f3f83d4ce8178988896416463eaf846d2
ajmedinabalboa <alvaro.medina.developer@gmail.com>
2025-01-08 21:12:56 -0400

    new Component GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Logo from './Components/Home/Logo.jsx'
-import Body from './Components/Home/Body.jsx'
-import Footer from './Components/Home/Footer.jsx'
+import Home from './screens/Home.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Logo/>
-      <Body/>
-      <Footer/>
+      <Home/>      
     </>
   )
 }

9ea1131cef3a9a07e47e3dc5ddd4b3940ea636dc
ajmedinabalboa <alvaro.medina.developer@gmail.com>
2025-01-09 21:40:22 -0400

    adding redux states 2

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../store/features/counter/counterSlice.js'
 
 import Body from '../Components/Home/Body.jsx'
 import Footer from '../Components/Home/Footer.jsx'
@@ -7,14 +10,16 @@ import viteLogo from "../assets/vite.svg"
 import reactLogo from "../assets/react.svg"
 import githubLogo from "../assets/github.svg"
 const Home = () => {
+    const count = useSelector((state) => state.counter.value)
     return (
         <div>         
-            
+            <div>Count value: <span>{count}</span></div>
+            <div>
             <OpenLink redirectURL="https://vite.dev" logoIMG={viteLogo} altDescription="VITE"/>
             <OpenLink redirectURL="https://react.dev" logoIMG={reactLogo} altDescription="REACT"/>
-            <OpenLink redirectURL="https://github.com/ajmedinabalboa/curso-de-react" logoIMG={githubLogo} altDescription="GITHUB"/>
-            <Body/>
-            <Footer/>
+            <OpenLink redirectURL="https://github.com/ajmedinabalboa/curso-de-react" logoIMG={githubLogo} altDescription="GITHUB"/>            
+            </div>
+            
         </div>        
     );
 };

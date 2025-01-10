b26b23e722c7c09a6e9bbc6ebb5711d08579a46c
ajmedinabalboa <alvaro.medina.developer@gmail.com>
2025-01-09 21:25:13 -0400

    adding redux states

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './screens/Home.jsx'
+import Counter from './Components/Counter/Counter.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Home/>      
+      <Counter/>
     </>
   )
 }

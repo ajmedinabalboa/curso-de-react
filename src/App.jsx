b00e97311dc4059237a086e4f45f10aa61c6f032
ajmedinabalboa <alvaro.medina.developer@gmail.com>
2025-01-14 21:15:16 -0400

    se adiciono el router react

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import './App.css'
 import Home from './screens/Home.jsx'
 import Counter from './Components/Counter/Counter.jsx'
 import Product from './screens/Product.jsx'
+
+import Navigator from "./Components/Navigation/Navigator.jsx";
+import Router from "./Components/Navigation/Router.jsx";
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Product/>  
+      <Navigator/>
+      <Router/>
     </>
+    
   )
 }
 
